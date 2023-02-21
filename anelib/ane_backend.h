1b00a27fe889f9f7dee4b51c17b7630d792032aa
Eileen Yoon <eyn@gmx.com>
2023-02-21 19:24:33 +0900

    anelib: conc backend
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- anelib/ane_backend.h
+++ anelib/ane_backend.h
@@ -4,7 +4,32 @@
 #ifndef __ANE_BACKEND_H__
 #define __ANE_BACKEND_H__
 
-#include "ane_anec.h"
+#define ANE_FIFO_WIDTH 0x400 // nxtpow2(0x274)
+#define ANE_FIFO_COUNT 0x20
+
+static inline void set_fifo_nid(void *td, int nid)
+{
+	uint32_t hdr0 = *(uint32_t *)td;
+	hdr0 = (hdr0 & 0xf00ffff) | (nid << 16);
+	memcpy(td, &hdr0, sizeof(uint32_t));
+	return;
+}
+
+static inline void ane_load_anec(struct ane_nn *nn, void *anec_data)
+{
+	const struct anec *anec = to_anec(nn);
+
+	memcpy(nn->chans[0], anec_data, anec->size);
+
+	/* do not fucking overflow */
+	memcpy(nn->fifo_chan, anec_data, anec->td_size);
+	memcpy(nn->fifo_chan + ANE_FIFO_WIDTH, anec_data, anec->td_size);
+
+	set_fifo_nid(nn->fifo_chan, FIFO_NID_MAGIC);
+	set_fifo_nid(nn->fifo_chan + ANE_FIFO_WIDTH,
+		     FIFO_NID_MAGIC + ANE_FIFO_COUNT);
+	return;
+}
 
 static int ane_free_chans(struct ane_nn *nn)
 {
@@ -93,22 +118,12 @@ static int ane_inst_backend(struct ane_nn *nn, void *anec_data)
 	err = ane_alloc_chans(nn);
 	if (err) {
 		fprintf(stderr, "ANELIB: failed to alloc chans, 0x%x\n", err);
-		goto free_data;
+		return err;
 	}
 
-	err = ane_init_anec_fromp(nn, anec_data);
-	if (err) {
-		fprintf(stderr, "ANELIB: failed to load anec backend, 0x%x\n",
-			err);
-		goto free_chans;
-	}
+	ane_load_anec(nn, anec_data);
 
 	return 0;
-
-free_chans:
-	ane_free_chans(nn);
-free_data:
-	return err;
 }
 
 static int ane_free_backend(struct ane_nn *nn)

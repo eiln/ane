3dd036edad381d89be717453ff47f6208e7820a9
Eileen Yoon <eyn@gmx.com>
2023-03-18 17:51:52 +0900

    python: fix setup.py
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='ane',
-   version='1.0.3',
-   description='ANE driver interface',
-   author='Eileen Yoon',
-   author_email='eyn@gmx.com',
-   packages=['ane'],  # same as name
-   install_requires=['numpy'], # external packages as dependencies
+	name='ane',
+	version='1.0.3',
+	description='ANE driver interface',
+	author='Eileen Yoon',
+	author_email='eyn@gmx.com',
+	packages=['ane'],
+	install_requires=['numpy'],
 )

6332753014b6c525f8204921ce5724fbc417b6ce
Eileen Yoon <eyn@gmx.com>
2023-03-23 11:00:03 +0900

    libane/ane_tile: improvements
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- libane/src/ane_tile.c
+++ libane/src/ane_tile.c
@@ -5,18 +5,18 @@
 #include "ane_dev.h"
 #include "ane_mem.h"
 
-static inline void ane_tile_v2(void *data, void *tile, const uint64_t N,
-			       const uint64_t C, const uint64_t H,
-			       const uint64_t W, const uint64_t P,
-			       const uint64_t R)
+static inline void ane_tile(void *data, void *tile, const uint64_t N,
+			    const uint64_t C, const uint64_t H,
+			    const uint64_t W, const uint64_t P,
+			    const uint64_t R)
 {
-	// skips tile zero set
+	/* skips zero-setting tile */
 	const uint64_t new_H = P / R;
 	const uint64_t new_W = R / sizeof(uint16_t);
 	const uint64_t stride = W * sizeof(uint16_t);
 
-	// const uint64_t tile_size = tile_align(N * C * P);
-	// memset(tile, 0, tile_size);
+	/* const uint64_t tile_size = tile_align(N * C * P); */
+	/* memset(tile, 0, tile_size); */
 
 	uint16_t(*data_a)[N][C][H][W] = (uint16_t(*)[N][C][H][W])data;
 	uint16_t(*tile_a)[N][C][new_H][new_W] =
@@ -34,10 +34,10 @@ static inline void ane_tile_v2(void *data, void *tile, const uint64_t N,
 	return;
 }
 
-static inline void ane_untile_v2(void *data, void *tile, const uint64_t N,
-				 const uint64_t C, const uint64_t H,
-				 const uint64_t W, const uint64_t P,
-				 const uint64_t R)
+static inline void ane_untile(void *data, void *tile, const uint64_t N,
+			      const uint64_t C, const uint64_t H,
+			      const uint64_t W, const uint64_t P,
+			      const uint64_t R)
 {
 	const uint64_t new_H = P / R;
 	const uint64_t new_W = R / sizeof(uint16_t);
@@ -50,7 +50,7 @@ static inline void ane_untile_v2(void *data, void *tile, const uint64_t N,
 	uint16_t(*tile_a)[N][C][new_H][new_W] =
 		(uint16_t(*)[N][C][new_H][new_W])tile;
 
-	for (uint64_t n = 0; n < N; n++) {
+	for (uint64_t n = 0; n < N; n++) { // TODO; early exit on alignment
 		for (uint64_t c = 0; c < C; c++) {
 			for (uint64_t h = 0; h < H; h++) {
 				memcpy((*data_a)[n][c][h], (*tile_a)[n][c][h],
@@ -66,13 +66,22 @@ int ane_tile_and_send(struct ane_nn *nn, void *from, const int idx)
 {
 	const struct ane_model *model = nn->model;
 	const int bdx = nn->src_bdx[idx];
+	void *tile = NULL;
 
-	const uint64_t tile_size = tile_shift(to_anec(nn)->tiles[bdx]);
-	void *tile = ane_zmemalign(tile_size);
+	if (idx >= input_count(nn)) {
+		fprintf(stderr, "LIBANE: invalid input index\n");
+		return -EINVAL;
+	}
 
-	ane_tile_v2(from, tile, model->nchw[bdx][0], model->nchw[bdx][1],
-		    model->nchw[bdx][2], model->nchw[bdx][3],
-		    model->nchw[bdx][4], model->nchw[bdx][5]);
+	tile = ane_zmemalign(tile_shift(to_anec(nn)->tiles[bdx]));
+	if (!tile) {
+		fprintf(stderr, "LIBANE: not enough mem to send input\n");
+		return -ENOMEM;
+	}
+
+	ane_tile(from, tile, model->nchw[bdx][0], model->nchw[bdx][1],
+		 model->nchw[bdx][2], model->nchw[bdx][3], model->nchw[bdx][4],
+		 model->nchw[bdx][5]);
 	ane_send(nn, tile, idx);
 
 	free(tile);
@@ -84,14 +93,23 @@ int ane_untile_and_read(struct ane_nn *nn, void *to, const int idx)
 {
 	const struct ane_model *model = nn->model;
 	const int bdx = nn->dst_bdx[idx];
+	void *tile = NULL;
 
-	const uint64_t tile_size = tile_shift(to_anec(nn)->tiles[bdx]);
-	void *tile = ane_zmemalign(tile_size);
+	if (idx >= output_count(nn)) {
+		fprintf(stderr, "LIBANE: invalid output index\n");
+		return -EINVAL;
+	}
+
+	tile = ane_zmemalign(tile_shift(to_anec(nn)->tiles[bdx]));
+	if (!tile) {
+		fprintf(stderr, "LIBANE: not enough mem to receive output\n");
+		return -ENOMEM;
+	}
 
 	ane_read(nn, tile, idx);
-	ane_untile_v2(to, tile, model->nchw[bdx][0], model->nchw[bdx][1],
-		      model->nchw[bdx][2], model->nchw[bdx][3],
-		      model->nchw[bdx][4], model->nchw[bdx][5]);
+	ane_untile(to, tile, model->nchw[bdx][0], model->nchw[bdx][1],
+		   model->nchw[bdx][2], model->nchw[bdx][3],
+		   model->nchw[bdx][4], model->nchw[bdx][5]);
 
 	free(tile);
 

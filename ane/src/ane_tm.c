bad6af5cb92c2c5fcec99b048666181011abb50b
Eileen Yoon <eyn@gmx.com>
2023-05-05 05:29:36 +0900

    ane/ane_tm.c: revert relaxed r/w
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- ane/src/ane_tm.c
+++ ane/src/ane_tm.c
@@ -54,25 +54,19 @@ static const int TQ_PRTY_TABLE[ANE_TQ_COUNT] = { 0x1, 0x2, 0x3,	 0x4,
 
 #define tm_read32(ane, off)	  (readl(ane->engine + ANE_TM_BASE + off))
 #define tq_read32(ane, off)	  (readl(ane->engine + ANE_TQ_BASE + off))
-
 #define tm_write32(ane, off, val) (writel(val, ane->engine + ANE_TM_BASE + off))
 #define tq_write32(ane, off, val) (writel(val, ane->engine + ANE_TQ_BASE + off))
 
-#define tm_write32_rel(ane, off, val) \
-	(writel_relaxed(val, ane->engine + ANE_TM_BASE + off))
-#define tq_write32_rel(ane, off, val) \
-	(writel_relaxed(val, ane->engine + ANE_TQ_BASE + off))
-
 void ane_tm_enable(struct ane_device *ane)
 {
-	tm_write32_rel(ane, TM_TQ_EN, tm_read32(ane, TM_TQ_EN) | 0x1000);
+	tm_write32(ane, TM_TQ_EN, tm_read32(ane, TM_TQ_EN) | 0x1000);
 
 	for (int qid = 0; qid < ANE_TQ_COUNT; qid++) {
-		tq_write32_rel(ane, TQ_PRTY(qid), TQ_PRTY_TABLE[qid]);
+		tq_write32(ane, TQ_PRTY(qid), TQ_PRTY_TABLE[qid]);
 	}
 
-	tm_write32_rel(ane, TM_IRQ_EN1, 0x4000000);
-	tm_write32_rel(ane, TM_IRQ_EN2, 0x6);
+	tm_write32(ane, TM_IRQ_EN1, 0x4000000);
+	tm_write32(ane, TM_IRQ_EN2, 0x6);
 }
 
 int ane_tm_enqueue(struct ane_device *ane, struct ane_request *req)
@@ -91,12 +85,12 @@ int ane_tm_enqueue(struct ane_device *ane, struct ane_request *req)
 	tq_write32(ane, TQ_STATUS(qid), 0x1);
 
 	for (int bdx = 0; bdx < ANE_TILE_COUNT; bdx++) {
-		tq_write32_rel(ane, TQ_BAR1(qid, bdx), req->bar[bdx]);
+		tq_write32(ane, TQ_BAR1(qid, bdx), req->bar[bdx]);
 	}
 
-	tq_write32_rel(ane, TQ_SIZE1(qid), ((req->td_size >> 2) - 1) << 0x10);
-	tq_write32_rel(ane, TQ_ADDR1(qid), req->fifo_addr);
-	tq_write32_rel(ane, TQ_NID1(qid), (req->nid & 0xff) << 8 | 1);
+	tq_write32(ane, TQ_SIZE1(qid), ((req->td_size >> 2) - 1) << 0x10);
+	tq_write32(ane, TQ_ADDR1(qid), req->fifo_addr);
+	tq_write32(ane, TQ_NID1(qid), (req->nid & 0xff) << 8 | 1);
 
 	return 0;
 }

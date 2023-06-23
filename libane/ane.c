a6bd39b21d0fdb37613f245df95b626a2a4ca1f2
Eileen Yoon <eyn@gmx.com>
2023-06-23 23:12:24 +0900

    libane: calc constants in tiling
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- libane/ane.c
+++ libane/ane.c
@@ -623,6 +623,7 @@ void __ane_read(struct ane_nn *nn, void *to, const uint32_t idx)
 	       tile_size(nn, dst_bdx(nn, idx)));
 }
 
+// clang-format off
 void ane_tile(void *data, void *tile, const uint64_t N, const uint64_t C,
 	      const uint64_t H, const uint64_t W, const uint64_t P,
 	      const uint64_t R)
@@ -631,18 +632,16 @@ void ane_tile(void *data, void *tile, const uint64_t N, const uint64_t C,
 	const uint64_t new_W = R / sizeof(uint16_t);
 	const uint64_t stride = W * sizeof(uint16_t);
 
+	const uint64_t C0 = C * H * W;
+	const uint64_t C1 = C * new_H * new_W;
+	const uint64_t H0 = H * W;
+	const uint64_t H1 = new_H * new_W;
+
 	for (uint64_t n = 0; n < N; n++) {
 		for (uint64_t c = 0; c < C; c++) {
 			for (uint64_t h = 0; h < H; h++) {
-				void *src = ((void *)(data)) +
-					    ((n * (C * H * W) + c * (H * W) +
-					      h * (W)) *
-					     sizeof(uint16_t));
-				void *dst =
-					((void *)(tile)) +
-					((n * (C * new_H * new_W) +
-					  c * (new_H * new_W) + h * (new_W)) *
-					 sizeof(uint16_t));
+				void *src = ((void *)(data)) + ((n * C0 + c * H0 + h * W) * sizeof(uint16_t));
+				void *dst = ((void *)(tile)) + ((n * C1 + c * H1 + h * new_W) * sizeof(uint16_t));
 				memcpy(dst, src, stride);
 			}
 		}
@@ -657,25 +656,24 @@ void ane_untile(void *data, void *tile, const uint64_t N, const uint64_t C,
 	const uint64_t new_W = R / sizeof(uint16_t);
 	const uint64_t stride = W * sizeof(uint16_t);
 
+	const uint64_t C0 = C * H * W;
+	const uint64_t C1 = C * new_H * new_W;
+	const uint64_t H0 = H * W;
+	const uint64_t H1 = new_H * new_W;
+
 	memset(data, 0, N * C * H * W * sizeof(uint16_t));
 
 	for (uint64_t n = 0; n < N; n++) {
 		for (uint64_t c = 0; c < C; c++) {
 			for (uint64_t h = 0; h < H; h++) {
-				void *src =
-					((void *)(tile)) +
-					((n * (C * new_H * new_W) +
-					  c * (new_H * new_W) + h * (new_W)) *
-					 sizeof(uint16_t));
-				void *dst = ((void *)(data)) +
-					    ((n * (C * H * W) + c * (H * W) +
-					      h * (W)) *
-					     sizeof(uint16_t));
+				void *src = ((void *)(tile)) + ((n * C1 + c * H1 + h * new_W) * sizeof(uint16_t));
+				void *dst = ((void *)(data)) + ((n * C0 + c * H0 + h * W) * sizeof(uint16_t));
 				memcpy(dst, src, stride);
 			}
 		}
 	}
 }
+// clang-format on
 
 static inline void ___ane_tile_send(struct ane_nn *nn, void *from,
 				    const uint32_t idx)
